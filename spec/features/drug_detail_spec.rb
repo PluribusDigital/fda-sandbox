b761642d5a084520ee2c68e9d2672d3f3bcecdc2
Ben Morris <git@benmorris.net>
2016-02-17 14:20:04 -0500

    demo

--- spec/features/drug_detail_spec.rb
+++ spec/features/drug_detail_spec.rb
@@ -16,14 +16,14 @@ feature "Drug Detail", js: true do
     expect(page).to have_content "SILDENAFIL CITRATE"
     # Effects 
     expect(page).to have_content "DYSPNOEA"
-  end # typeahead
+  end 
 
   scenario "info on recalls" do
     visit "/#/drug/#{@viagra.product_ndc}" 
     expect(page).to have_content "Recalls: 0"
     visit "/#/drug/#{@advilpm.product_ndc}" 
     expect(page).to have_content "Recalls: 1"
-  end # typeahead
+  end 
 
   scenario "links to alternative drugs" do
     visit "/#/drug/#{@viagra.product_ndc}"
@@ -31,7 +31,7 @@ feature "Drug Detail", js: true do
     # page.find(:a, "Revatio").first.click
     first(:link, "Revatio").click
     expect(page).to have_content "Revatio"
-  end # typeahead
+  end 
 
 end 
 
