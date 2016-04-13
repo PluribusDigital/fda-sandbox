cc48fb0c91b6a95c50c58445b78e2f3e114e4ee9
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2016-04-13 16:22:29 -0400

    Fixing production SSL Certs issue

--- deploy/init/initdb.sh
+++ deploy/init/initdb.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+
+# let the DB launch
+sleep 10
+
+# build the DB
 cd /home/app/webapp
-bundle exec rake db:create
-bundle exec rake db:migrate
-#bundle exec rake db:seed
+bundle exec rake db:create && \
+bundle exec rake db:migrate && \
+bundle exec rake db:seed
